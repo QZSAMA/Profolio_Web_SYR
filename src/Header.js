9aa0d3d7cb1ff2c797500695623785e14e0deb50
Qz <KELVINCHAO1996@GMAIL.COM>
2022-07-30 17:32:22 -0400

    Add Bottom Line for Header Links
    Add Bottom Line for Header Links

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {useState} from 'react';
+
+// import styles from "./index.css"
 // import styles from './assets/styles.scss';
 // import './assets/Header.css';
 import logo from './logo.png';
@@ -7,61 +10,57 @@ import Container from 'react-bootstrap/Container';
 // import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { render } from '@testing-library/react';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 // import { LinkContainer } from 'react-router-bootstrap';
 
+const tabs = [
+    {
+      id: 1,
+      href: '/',
+      text:'Work',
+    },
+    {
+      id: 2,
+      href: '/about',
+      text:'About'
+    },
+];
+
+
+
 class Header extends React.Component{
-    render(){  
+    
+    render(){
         return(
-            <Container>
-                <Navbar collapseOnSelect expand="md" variant="dark" className='nav-header d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start' >
-                    <Navbar.Brand className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none" href="/" >
-                        <img src={logo} alt="Yiran Sun"/>         
-                    </Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav"  className="nav col-12 col-lg-auto my-2 justify-content-end my-md-0 text-small ">
-                        <Nav className="mt-2 mt-md-0 nav-header text-white">
-                            <Nav.Link className="l navlink " href="/">Works</Nav.Link>
-                            <Nav.Link className="l navlink" href="/about">About</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                {/* <Navbar expand="md"  variant="dark"  >
-                    
-                </Navbar> */}
-            </Container>
-                   
-                    
-            // <>
-            //     <ButtonToolbar>
-            //         <Button bsStyle="primary" bsSize="xsmall">Large</Button>
-            //     </ButtonToolbar>
-            //     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            //         <Container>
-            //             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            //             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            //             <Navbar.Collapse id="responsive-navbar-nav">
-            //                 <Nav className="me-auto">
-            //                 <Nav.Link href="#features">Features</Nav.Link>
-            //                 <Nav.Link href="#pricing">Pricing</Nav.Link>
-            //                 <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            //                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            //                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            //                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            //                     <NavDropdown.Divider />
-            //                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            //                 </NavDropdown>
-            //                 </Nav>
-            //                 <Nav>
-            //                 <Nav.Link href="#deets">More deets</Nav.Link>
-            //                 <Nav.Link eventKey={2} href="#memes">
-            //                     Dank memes
-            //                 </Nav.Link>
-            //                 </Nav>
-            //             </Navbar.Collapse>
-            //         </Container>
-            //     </Navbar>
-            // </>
+            <ToggleButton/>
         )
     }
+}
+function ToggleButton(){
+    const [selected, setSelected] = React.useState(null)
+    return (
+        <>
+        
+        <Container>
+            <Navbar collapseOnSelect expand="md" variant="dark" className='nav-header d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start' >
+                <Navbar.Brand className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none" href="/" >
+                    <img src={logo} alt="Yiran Sun"/>         
+                </Navbar.Brand>
+                <Navbar.Collapse id="responsive-navbar-nav"  className="nav col-12 col-lg-auto my-2 justify-content-end my-md-0 text-small ">
+                    <Nav className="mt-2 mt-md-0 nav-header">
+                    {tabs.map((item , index) => (
+                        <Nav.Link href={item.href} className={"l navlink  text-white "+(selected===index?'selected':null)} onPress={()=>setSelected(index)}>{item.text}</Nav.Link>
+                            
+                    ))}
+                        {/* <Nav.Link className="l navlink  text-white active" href="/">Work</Nav.Link>
+                        <Nav.Link className="l navlink text-white " href="/about">About</Nav.Link> */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        
+        </Container>
+        </>
+    );
 }
 export default Header;
--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,17 @@ function Home(){
         <div className="bg-dark text-white">
             <Header/>
             <h6 className='text-center l'>Choose to browse projects</h6>
-            <div class="box">
-                <div class="card">卡片1</div>
-                <div class="card">卡片2</div>
-                <div class="card">卡片3</div>
-                <div class="card">卡片4</div>
-                <div class="card">卡片5</div>
-                <div class="card">卡片6</div>
+            <div className="box">
+                <div className="card">卡片1</div>
+                <div className="card">卡片2</div>
+                <div className="card">卡片3</div>
+                <div className="card">卡片4</div>
+                <div className="card">卡片5</div>
+                <div className="card">卡片6</div>
             </div>
             <h6 className='text-center l'>Illustration</h6>
-                <div class="card">卡片1</div>
-                <div class="card">卡片2</div>
+                <div className="card">卡片1</div>
+                <div className="card">卡片2</div>
             <h6 className='text-center l'>Other Works</h6>
         </div>
   );
